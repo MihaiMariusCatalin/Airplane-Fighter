fd3e51a1b734625dd5327d20f96b692db673f268
MihaiMariusCatalin <167853612+MihaiMariusCatalin@users.noreply.github.com>
2024-09-02 17:10:14 +0300

    Update airplane.js

--- airplane.js
+++ airplane.js
@@ -1,26 +1,46 @@
-const gameBoard = document.querySelector("#gameBoard");
-const ctx = gameBoard.getContext("2d");
-const gameWidth = gameBoard.width;
-const gameHeight = gameBoard.height;
+const game = document.getElementById("gameBoard");
+const ctx = game.getContext("2d");
 const unitSize = 25;
+
 const backgroundColor = "black";
-const planeColor = "lightblue";
-const objectColor = "white";
-let gameOver = false;
-let objects = [];
+const planeColor = "blue";
+const objColor = "white";
 
 let plane = {
-    x : gameHeight / 2 - unitSize,
-    y : gameWidth - unitSize * 2,
-    width : unitSize,
-    height : unitSize
+    x : (game.width - unitSize) / 2,
+    y : game.height - (unitSize * 2),
+    speed : 20,
+    height : unitSize,
+    width : unitSize
+}
+let objects = [];
+let seconds = 0;
+let gameOver = false;
+
+function gameLoop() {
+    if (gameOver) {
+        document.getElementById("gameScore").innerText = seconds;
+        return;
+    }
+    ctx.clearRect(0, 0, game.height, game.width);
+    drawPlane();
+    drawObj();
+    updateObjects();
+    checkCollision();
 }
 
-window.onload = function() {
-    drawPlane();
-    document.addEventListener("keydown", movePlane);
+setInterval(increaseScore, 1000);
+setInterval(gameLoop, 1000/60);
+window.addEventListener("keydown", movePlane);
+window.onload = function () {
     generateObjects();
-    moveObjects();
+}
+
+function increaseScore() {
+    if (gameOver) {
+        return;
+    }
+    ++seconds;
 }
 
 function drawPlane() {
@@ -28,42 +48,62 @@ function drawPlane() {
     ctx.fillRect(plane.x, plane.y, plane.height, plane.width);
 }
 
-function movePlane(e) {
-    if (e.code == "ArrowLeft" && plane.x > 0) {
-        plane.x -= unitSize;
-    } else if (e.code == "ArrowRight" && plane.x < gameHeight - unitSize) {
-        plane.x += unitSize;
+function drawObj() {
+    for (let obj of objects) {
+        ctx.fillStyle = objColor;
+        ctx.fillRect(obj.x, obj.y, obj.height, obj.width);
     }
-    ctx.clearRect(0, 0, gameHeight, gameWidth);
-    drawPlane();
-    drawObjects();
+}
+
+function movePlane(e) {
+    if(e.code == "ArrowRight" && plane.x < game.width - unitSize) {
+        plane.x += plane.speed;
+    }
+    if (e.code == "ArrowLeft" && plane.x > 0) {
+        plane.x -= plane.speed;
+    }
 }
 
 function generateObjects() {
     for (let i = 0; i < 5; ++i) {
         let obj = {
-            x : Math.floor(Math.random() * gameWidth) - unitSize,
+            x : Math.floor(Math.random() * game.width + unitSize),
             y : 0,
-            width : unitSize,
-            height : unitSize
-        };
+            speed : Math.floor(Math.random() * 3 + 1),
+            height : unitSize,
+            width : unitSize
+        }
         objects.push(obj);
     }
 }
 
-function moveObjects() {
-    for(let obj of objects) {
-        while (obj.y <= 300) {
-            obj.y += 1;
-            ctx.clearRect(0, 0, gameHeight, gameWidth);
-            drawPlane();
-            drawObjects();
+function updateObjects() {
+    for (let i = 0; i < 5; ++i) {
+        if (objects[i].y < game.height) {
+            objects[i].y += objects[i].speed;
+        } else {
+            objects[i].x = Math.floor(Math.random() * game.width + unitSize);
+            objects[i].y = 0;
         }
     }
 }
-function drawObjects() {
-    for(let obj of objects) {
-        ctx.fillStyle = objectColor;
-        ctx.fillRect(obj.x, obj.y, obj.height, obj.width);
+
+function checkCollision() {
+    for (let obj of objects) {
+        if (
+            obj.x < plane.x + plane.width &&
+            obj.x + obj.width > plane.x &&
+            obj.y < plane.y + plane.height &&
+            obj.y + obj.height > plane.y) {
+            
+            gameOver = true;
+        }
     }
+}
+
+function resetGame() {
+    gameOver = false;
+    document.getElementById("gameScore").innerHTML = "";
+    objects = [];
+    generateObjects();
 }
